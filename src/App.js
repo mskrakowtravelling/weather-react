990ab66106a43f3ac2c586003a62aa3e91528edf
mskrakowtravelling <104271065+mskrakowtravelling@users.noreply.github.com>
2022-10-03 13:10:40 +0200

    removed redundant files and css , changed the main title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import Weather from './Weather';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello!</h1>
-        <Weather />
-      </header>
+      <h1>Hello!</h1>
     </div>
   );
 }
